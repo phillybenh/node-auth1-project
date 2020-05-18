3c784debdc5df4b78d667bd2fb701356e17a2d0c
Ben Haus <ben@little-haus.com>
2020-05-18 18:28:57 -0400

    stretch app form started

--- stretch-app/src/App.js
+++ stretch-app/src/App.js
@@ -1,24 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+const initialState = {
+  username: "",
+  password: "",
+};
+
 function App() {
+
+  const [login, setLogin] = useState(initialState);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setLogin({
+      ...login,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const userLogin = (e) => {
+    e.preventDefault();
+    // props.loginAction(login)
+    // axiosWithAuth()
+    //   .post("/login", login)
+    //   .then((res) => {
+    //     localStorage.setItem("token", JSON.stringify(res.data.token));
+    //     localStorage.setItem("userID", res.data.user.id);
+    //     console.log({ res });
+    //     props.loginAction(res);
+    //     push("/main");
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //     alert("Please enter a valid username and password, or register a new account.")
+    //   });
+  };
+  const userRegister = (e) => {
+    e.preventDefault();
+
+    // axiosWithAuth()
+    //   .post("/register", login)
+    //   .then((res) => {
+    //     localStorage.setItem("token", JSON.stringify(res.data.token));
+    //     localStorage.setItem("userID", res.data.user.id);
+
+    //     console.log({ res });
+    //     props.registerAction(res);
+    //     push("/userProfile");
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //     alert("There was an error. Please try again.")
+    //     // console.log({err})
+    //   });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Stretch App</h1>
       </header>
+      <h3 className='login'> Login or Register</h3>
+      <form>
+        <input
+          label="Username"
+          type="text"
+          name="username"
+          placeholder="username"
+          value={login.username}
+          onChange={handleChange}
+        />
+        <br />
+        <input
+          label="Password"
+          type="password"
+          name="password"
+          placeholder="password"
+          value={login.password}
+          onChange={handleChange}
+        />
+
+        <button onClick={userLogin}>Login</button>
+        <button onClick={userRegister}>Register</button>
+      </form>
     </div>
   );
 }
